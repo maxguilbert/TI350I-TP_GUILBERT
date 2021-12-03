edf8421cb61a1608e0ee8d2a6c1e211a10b0ae7f
maxguilbert <94389559+maxguilbert@users.noreply.github.com>
2021-12-03 15:58:38 +0100

    Update sonarQube01.java
    put "package main;" in first line and replace "s" by "S" in second line make at 15 : 58

--- sonarQube01.java
+++ sonarQube01.java
@@ -1,5 +1,5 @@
-
-public class sonarQube01 {
+package main;
+public class SonarQube01 {
     String addHeader(String stringToAdd, int x, int y){
         return String.format("Added Header %s %s %s", stringToAdd, x, y);
     }
